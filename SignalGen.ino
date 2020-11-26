f9b60ee4463dedc84e9289918049c124f28ecf7b
Tim Boldt <tim.boldt@gmail.com>
2020-11-25 22:39:06 -0800

    Basic 300-ish Hz sine-cosine output.

--- SignalGen.ino
+++ SignalGen.ino
@@ -11,14 +11,7 @@ bool on = false;
 void loop() {
     double x = sin(theta);
     double y = cos(theta);
-    analogWrite(A0, (uint32_t)round(x*2048 + 2048));
-    analogWrite(A1, (uint32_t)round(y*2048 + 2048));
-    theta += 0.001;  // 1 milliradian
-    // if (v >= 1<<12) {
-    //     v = 0;
-    //     // LOW is red LED on.
-    //     on = !on;
-    //     digitalWrite(13, on ? LOW : HIGH);
-    // }
-    delayMicroseconds(1);
+    analogWrite(A0, (uint32_t)round(x*2047 + 2048));
+    analogWrite(A1, (uint32_t)round(y*2047 + 2048));
+    theta += 0.01;
 }
