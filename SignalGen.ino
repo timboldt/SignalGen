6632b19c6701d33dbafd44fd061e4281ace35fd4
Tim Boldt <tim.boldt@gmail.com>
2020-11-25 23:38:35 -0800

    Switch to q31 and Arm math.

--- SignalGen.ino
+++ SignalGen.ino
@@ -1,17 +1,21 @@
 #include <Arduino.h>
+#include <arm_math.h>
 
 void setup() {
     pinMode(13, OUTPUT);
     analogWriteResolution(12);
 }
 
-float theta = 0;
-bool on = false;
+// If theta advances by this much per loop cycle on a Feather M4, with default
+// arduino-cli compilation settings, it will generate a 1Hz output.
+const q31_t one_hz = 20548;
+
+q31_t theta = 0;
 
 void loop() {
-    float x = sin(theta);
-    float y = cos(theta);
-    analogWrite(A0, (uint32_t)round(x*2047 + 2048));
-    analogWrite(A1, (uint32_t)round(y*2047 + 2048));
-    theta += 0.1;
+    q31_t x, y;
+    arm_sin_cos_q31(theta, &x, &y);
+    analogWrite(A0, x / 1024 / 1024 + 2048);
+    analogWrite(A1, y / 1024 / 1024 + 2048);
+    theta += one_hz * 440;
 }
