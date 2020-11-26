37d18661403adf68ada103f6276fa08d579f0dd7
Tim Boldt <tim.boldt@gmail.com>
2020-11-25 23:00:37 -0800

    Switch to single-precision floating point.

--- SignalGen.ino
+++ SignalGen.ino
@@ -5,13 +5,13 @@ void setup() {
     analogWriteResolution(12);
 }
 
-double theta = 0;
+float theta = 0;
 bool on = false;
 
 void loop() {
-    double x = sin(theta);
-    double y = cos(theta);
+    float x = sin(theta);
+    float y = cos(theta);
     analogWrite(A0, (uint32_t)round(x*2047 + 2048));
     analogWrite(A1, (uint32_t)round(y*2047 + 2048));
-    theta += 0.01;
+    theta += 0.1;
 }
